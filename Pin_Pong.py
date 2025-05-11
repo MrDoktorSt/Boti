149e5ba5e9a39dfa87f3ae67bf099b3ffcf15d26
MrDoktorSt <sasha.ly20099@gmail.com>
2025-05-11 15:13:26 +0300

    Update Pin_Pong.py

--- Pin_Pong.py
+++ Pin_Pong.py
@@ -1,5 +1,7 @@
 from pygame import *
 
+
+
 class GameSprite(sprite.Sprite):
    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
        super().__init__()
@@ -30,11 +32,12 @@ class Player(GameSprite):
 
 
 
-back = (200, 255, 255) #цвет фона (background)
+back = (200, 255, 255) 
 win_width = 600
 win_height = 500
 window = display.set_mode((win_width, win_height))
 window.fill(back)
+
 
 
 game = True
@@ -43,9 +46,9 @@ clock = time.Clock()
 FPS = 60
 
 
-  
-racket1 = Player('racket.png', 30, 200, 4, 50, 150) 
-racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+
+racket1 = Player('racket.png', 30, 200, 4, 50, 200) 
+racket2 = Player('racket.png', 520, 200, 4, 50, 200)
 ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
 
 
@@ -55,8 +58,8 @@ lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
 lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
 
 
-speed_x = 3
-speed_y = 3
+speed_x = 6
+speed_y = 6
 
 
 while game:
@@ -76,19 +79,18 @@ while game:
            speed_x *= -1
            speed_y *= 1
       
-       
+      
        if ball.rect.y > win_height-50 or ball.rect.y < 0:
            speed_y *= -1
 
 
-       
        if ball.rect.x < 0:
            finish = True
            window.blit(lose1, (200, 200))
            game_over = True
 
 
-       
+      
        if ball.rect.x > win_width:
            finish = True
            window.blit(lose2, (200, 200))
